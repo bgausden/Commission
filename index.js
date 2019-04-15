37c0f1624697a5097e0d6f87e15b0cb175d25bef
Barry Gausden <github@gausden.org>
2019-04-15 19:19:09 +0800

    EOD 20190415

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 "use strict";
-/*
-read the excel file
-find employee lines
-foreach employee
-    check for commission structure e.g hurdle
-    calculate services commission
-generate report
-push into talenox
-*/
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -16,14 +7,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-// Button callback
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const xlsx_1 = __importDefault(require("xlsx"));
+const prettyjson_1 = __importDefault(require("prettyjson"));
+/*
+pass excel file name as parameter
+read the excel file
+find employee lines by locating line beginning with "Stylists"
+foreach employee
+    check for commission structure e.g hurdle
+    calculate services commission
+generate report
+push into talenox
+*/
+// Button callback if we're doing things via a web page
 function onButtonClicked() {
     return __awaiter(this, void 0, void 0, function* () {
         return yield selectFile("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     });
 }
 /**
- * Select file(s).
+ * Select file(s). If we're doing things via a web page.
  * @param {String} contentType The content type of files you wish to select. For instance "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" to select an Excel file.
  * @returns {Promise<File|File[]>} A promise of a file or array of files in case the multiple parameter is true.
  */
@@ -45,3 +52,10 @@ function selectFile(contentType) {
         input.click();
     });
 }
+const filePath = "/Users/barryga/Documents/Sample Payroll Report.xlsx";
+let workbook = xlsx_1.default.readFile(filePath);
+console.log(workbook.SheetNames);
+let sheetName = workbook.SheetNames[0];
+console.log(sheetName);
+let wbJSON = xlsx_1.default.utils.sheet_to_json(workbook.Sheets[sheetName]);
+console.log(prettyjson_1.default.render(wbJSON));
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
+import XLSX from "xlsx";
+import prettyJSON from "prettyjson";
 /* 
+pass excel file name as parameter
 read the excel file
-find employee lines
+find employee lines by locating line beginning with "Stylists"
 foreach employee
     check for commission structure e.g hurdle
     calculate services commission
@@ -8,7 +11,7 @@ generate report
 push into talenox
 */
 
-// Button callback
+// Button callback if we're doing things via a web page
 async function onButtonClicked() {
     return await selectFile(
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
@@ -16,12 +19,12 @@ async function onButtonClicked() {
 }
 
 /**
- * Select file(s).
+ * Select file(s). If we're doing things via a web page.
  * @param {String} contentType The content type of files you wish to select. For instance "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" to select an Excel file.
  * @returns {Promise<File|File[]>} A promise of a file or array of files in case the multiple parameter is true.
  */
 function selectFile(contentType: string) {
-    return new Promise(resolve => {
+    return new Promise<File>(resolve => {
         let input = document.createElement("input");
         input.type = "file";
         input.accept = contentType;
@@ -35,7 +38,14 @@ function selectFile(contentType: string) {
                 resolve();
             }
         };
-
         input.click();
     });
 }
+
+const filePath: string = "/Users/barryga/Documents/Sample Payroll Report.xlsx";
+let workbook = XLSX.readFile(filePath);
+console.log(workbook.SheetNames);
+let sheetName = workbook.SheetNames[0];
+console.log(sheetName);
+let wbJSON = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
+console.log(prettyJSON.render(wbJSON));
