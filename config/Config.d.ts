22891fcfad404252ffa5c67a51c6f5ff29ffb0cf
Barry Gausden <barry@lissome.hk>
2021-11-11 15:22:01 +0800

    Move api token to dotenv * add dotenv npm package * update launch config to immediately process .env * include Config.d.ts in the repo, remove talenox_api from dictionary * remove talenox_api from default.json (default config) * source talenox_api from process.env instead of from config * add debugging to talenox_functions getTalenoxStaff

---
+++ config/Config.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+interface Config {
+  PAYROLL_WB_NAME: string
+  PAYROLL_MONTH: string
+  PAYROLL_YEAR: string
+  PAYMENTS_WB_NAME: string
+  PAYMENTS_WS_NAME: string
+  missingStaffAreFatal: boolean
+  updateTalenox: boolean
+}
