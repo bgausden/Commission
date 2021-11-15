fea157a9c640d2b60117b4b22f385a89d8dd4f56
Barry Gausden <barry@lissome.hk>
2021-11-15 18:23:27 +0800

    20211115 WIP * create new log files for each run * date stamp log files * fix VS Code not building on run (tasks.json) * update build scripts to copy logging config to correct location

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-tsc -p tsconfig.json && cp ./src/staffHurdle.json ./log4js.json ./dist/
+cp ./log4js.json ./src/ && tsc -p tsconfig.json && cp ./src/staffHurdle.json ./src/log4js.json ./dist/
--- src/logging_functions.ts
+++ src/logging_functions.ts
@@ -1,11 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
 // import debug from 'debug'
+import { promises } from 'fs'
+const { readFile } = promises // node 14 doesn't support import {readFile} from 'fs/promises' so this work-around
 import l4js from 'log4js'
 const { configure, getLogger } = l4js
-import { extname } from 'path'
+import { Configuration } from 'log4js'
+import { basename, extname } from 'path'
 
 // the template in the root of the project needs to be copied to /src
 // the npm script "build" does this for us (see /scripts)
-import log4jsBaseConfig from "./log4js.json"
+let log4jsConfigFile = './log4js.json'
+if (process.env.log4jsConfigFile) {
+    log4jsConfigFile = process.env.log4jsConfigFile
+}
+
+const log4jsConfig: Configuration = JSON.parse(await readFile(new URL(`./${log4jsConfigFile}`, import.meta.url), { encoding: 'utf-8' }))
 
 export const debugLogger = getLogger('debug')
 debugLogger.level = 'debug'
@@ -22,26 +31,28 @@ errorLogger.level = 'error'
 export function shutdownLogging(): void {
     l4js.shutdown()
 }
-function fileNameSPlit(filename: string): string[] {
 
-    // TODO return an array or a tuple containing basename and extension 
-    // TODO gracefully handle no extension
+// TODO put this into an init() function
+// TODO create the contractor and commission log files as part of init()
 
-    const ext = extname(filename)
-    if (ext = '.') {
-        // file ends in "."?
-        return []
-    }
-}
+// change the filenames we'll log to by appending the datetime to the file names
+// @ts-ignore ts-2339
+const commissionLogFileName = log4jsConfig.appenders?.commission?.filename ?? './commission.log'
+const commissionLogFileExt = extname(commissionLogFileName)
+const commissionLogFileBaseName = basename(commissionLogFileName, commissionLogFileExt)
+let date = (new Date()).toISOString()
+// @ts-ignore ts-2339
+log4jsConfig.appenders.commission.filename = `log4jsConfig.appenders.commission.${commissionLogFileBaseName}-${date}${commissionLogFileExt}`
 
-// change the filename we'll log to by appending the datetime to the file name
-const date = Date.now()
-// find the basename of the file (the bit that's not the extension)
+// @ts-ignore ts-2339
+const contractorLogFileName = log4jsConfig.appenders?.contractor?.filename ?? './contractor.log'
+const contractorLogFileExt = extname(contractorLogFileName)
+const contractorLogFileBaseName = basename(contractorLogFileName, contractorLogFileExt)
+date = (new Date()).toISOString()
+// @ts-ignore ts-2339
+log4jsConfig.appenders.commission.filename = `log4jsBaseConfig.appenders.contractor.${contractorLogFileBaseName}-${date}${contractorLogFileExt}`
 
-
-log4jsBaseConfig.appenders.commission.filename = `log4jsBaseConfig.appenders.commission.filename`
-
-// configure('./log4js.json')
+configure(log4jsConfig)
 
 export const commissionLogger = getLogger("commission")
 commissionLogger.level = 'info'
