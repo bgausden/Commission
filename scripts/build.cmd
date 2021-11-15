b26060167c70718456d87ec9d58acd3f94313d0b
Barry Gausden <barry@lissome.hk>
2021-11-15 18:23:27 +0800

    20211115 WIP * create new log files for each run * date stamp log files * fix VS Code not building on run (tasks.json) * update build scripts to copy logging config to correct location

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -1,1 +1,4 @@
-tsc -p tsconfig.json && copy .\src\staffHurdle.json .\log4js.json .\dist\
+copy .\log4js.json .\src\
+call tsc -p tsconfig.json
+copy .\src\staffHurdle.json .\dist\
+copy .\src\log4js.json .\dist\
