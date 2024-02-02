3f82cf95b17823388c252b11487c6752fca79076
Barry Gausden <barry@lissome.hk>
2024-02-02 23:56:25 +0800

    Improvements to directory validation and moving/compressing old data files * minor changes to config schema - payroll_wb_filename is now only the filename * change string constants to "as const" * check that the data dir is valid * move (and compress) old payroll xlsx files into data/old, logs/old * move fn to move files to old to utility_functions.ts and extend to optionally retain some of the more recent files in the source dir

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
-const DEFAULT_PAYMENTS_DIR='payments'
-const DEFAULT_LOGS_DIR='logs'
-const DEFAULT_DATA_DIR='data'
+const DEFAULT_PAYMENTS_DIR='payments' as const
+const DEFAULT_LOGS_DIR='logs' as const
+const DEFAULT_DATA_DIR='data' as const
+const DEFAULT_OLD_DIR='old' as const
 
-export {DEFAULT_DATA_DIR, DEFAULT_LOGS_DIR, DEFAULT_PAYMENTS_DIR}
+export {DEFAULT_DATA_DIR, DEFAULT_LOGS_DIR, DEFAULT_PAYMENTS_DIR, DEFAULT_OLD_DIR}
