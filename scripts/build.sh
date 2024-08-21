a02b8663b0a3278c6a58f3b52e1001afcdd353e2
Barry Gausden <barry@lissome.hk>
2024-08-21 13:47:57 +0800

    Make build on posix more robust * ensure that if tsc isn't on the path, we use the one under node_modules (if any).

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,13 @@
-cp ./log4js.json ./src/ && tsc -p tsconfig.json && cp ./src/staffHurdle.json ./src/log4js.json ./dist/
+if ! command -v tsc &> /dev/null
+then
+    if [ -f "./node_modules/.bin/tsc" ]; then
+        TSC="./node_modules/.bin/tsc"
+    else
+        echo "tsc could not be found"
+        exit 1
+    fi
+else
+    TSC="tsc"
+fi
+
+cp ./log4js.json ./src/ && $TSC -p tsconfig.json && cp ./src/staffHurdle.json ./src/log4js.json ./dist/
