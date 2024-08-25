2bc2207a79a3a25507ac16952d3a707e08b0baae
Barry Gausden <barry@lissome.hk>
2024-08-25 12:07:27 +0800

    Make payGroup optional for payrollPayments

--- src/ITalenoxPayrollPayment.ts
+++ src/ITalenoxPayrollPayment.ts
@@ -1,13 +1,12 @@
-/* eslint-disable */
 export interface ITalenoxPayroll {
-    employee_ids: string[]
-    payment: TalenoxPayrollPayment
+  employee_ids: string[]
+  payment: TalenoxPayrollPayment
 }
 
 export interface TalenoxPayrollPayment {
-    year: string
-    month: string
-    period: string
-    pay_group: string
-    with_pay_items: boolean
+  year: string
+  month: string
+  period: string
+  pay_group?: string | null
+  with_pay_items: boolean
 }
