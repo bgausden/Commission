274669a57868ee8b0eac29debe109122c88dc664
Barry Gausden <barry@lissome.hk>
2024-10-07 20:01:10 +0800

    Initial Commit

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,26 @@
-const DEFAULT_PAYMENTS_DIR='payments' as const
-const DEFAULT_LOGS_DIR='logs' as const
-const DEFAULT_DATA_DIR='data' as const
-const DEFAULT_OLD_DIR='old' as const
+import { loadJsonFromFile } from './utility_functions.js'
 
-export {DEFAULT_DATA_DIR, DEFAULT_LOGS_DIR, DEFAULT_PAYMENTS_DIR, DEFAULT_OLD_DIR}
+const DEFAULT_PAYMENTS_DIR = 'payments' as const
+const DEFAULT_LOGS_DIR = 'logs' as const
+const DEFAULT_DATA_DIR = 'data' as const
+const DEFAULT_OLD_DIR = 'old' as const
+const DEFAULT_CONTRACTOR_LOGFILE = 'contractor.log'
+const DEFAULT_COMMISSION_LOGFILE = 'commission.log'
+
+const defaultStaffID = '000'
+
+const staffHurdle = loadJsonFromFile('dist/staffHurdle.json')
+
+const defaultLog4jsConfigFile = 'log4js.json'
+
+export {
+  DEFAULT_DATA_DIR,
+  DEFAULT_LOGS_DIR,
+  DEFAULT_PAYMENTS_DIR,
+  DEFAULT_OLD_DIR,
+  defaultStaffID,
+  staffHurdle,
+  defaultLog4jsConfigFile,
+  DEFAULT_COMMISSION_LOGFILE,
+  DEFAULT_CONTRACTOR_LOGFILE,
+}
