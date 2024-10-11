e468af8a254e755f709d99d7426377bd81f4611a
Barry Gausden <barry@lissome.hk>
2024-10-12 00:24:50 +0800

    Refactor to avoid import side-effects breaking vitest * remove fixtures in favour of memfs * now need to explicitly load staffHurdle.json (in entrypoint)

--- src/staffHurdle.ts
+++
@@ -1,8 +0,0 @@
-import { type StaffHurdle } from './IStaffHurdle.js'
-import { loadJsonFromFile } from './utility_functions.js'
-
-export function loadStaffHurdle() {
-  staffHurdle = loadJsonFromFile<StaffHurdle>('./dist/staffHurdle.json')
-}
-
-export declare var staffHurdle: StaffHurdle
