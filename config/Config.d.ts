c711ec993a4cd96d08d7e4cb97d2651fb6271eb5
Barry Gausden <barry@lissome.hk>
2025-01-28 20:46:29 +0000

    Jan 2025 Payroll

--- config/Config.d.ts
+++ config/Config.d.ts
@@ -1,9 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
 declare module "node-config-ts" {
   interface IConfig {
-    PAYROLL_WB_FILENAME: string;
-    missingStaffAreFatal: boolean;
-    updateTalenox: boolean;
+    PAYROLL_WB_FILENAME: string
+    missingStaffAreFatal: boolean
+    updateTalenox: boolean
   }
-  export const config: Config;
-  export type Config = IConfig;
+  export const config: Config
+  export type Config = IConfig
 }
